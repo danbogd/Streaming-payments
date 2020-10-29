0ae8016c2fb151e291b0fd74da668d65481fad63
danbogd <bogdanovdv1@mail.ru>
2020-10-29 15:19:11 +0300

    remove redudant code

--- My_stream.sol
+++ My_stream.sol
@@ -57,119 +57,136 @@ library Roles {
     }
 }
 
-// инициализация
-contract Initializable {
 
-  /**
-   * @dev Indicates that the contract has been initialized.
-   */
-  bool private initialized;
-
-  /**
-   * @dev Indicates that the contract is in the process of being initialized.
-   */
-  bool private initializing;
-
-  /**
-   * @dev Modifier to use in the initializer function of a contract.
-   */
-  modifier initializer() {
-    require(initializing || isConstructor() || !initialized, "Contract instance has already been initialized");
-
-    bool isTopLevelCall = !initializing;
-    if (isTopLevelCall) {
-      initializing = true;
-      initialized = true;
-    }
-
-    _;
-
-    if (isTopLevelCall) {
-      initializing = false;
-    }
-  }
-
-  /// @dev Returns true if and only if the function is running in the constructor
-  function isConstructor() private view returns (bool) {
-    // extcodesize checks the size of the code stored in an address, and
-    // address returns the current address. Since the code is still not
-    // deployed when running a constructor, any checks on its code size will
-    // yield zero, making it an effective way to detect if a contract is
-    // under construction or not.
-    uint256 cs;
-    assembly { cs := extcodesize(address) }
-    return cs == 0;
-  }
-
-
-}
-
-// контекст от инициализации
-contract Context is Initializable {
+/*
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with GSN meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+contract Context {
     // Empty internal constructor, to prevent people from mistakenly deploying
-    // an instance of this contract, with should be used via inheritance.
+    // an instance of this contract, which should be used via inheritance.
     constructor () internal { }
-    // solhint-disable-previous-line no-empty-blocks
 
-    function _msgSender() internal view returns (address) {
+    function _msgSender() internal view  returns (address payable) {
         return msg.sender;
     }
 
-    function _msgData() internal view returns (bytes memory) {
+    function _msgData() internal view  returns (bytes memory) {
         this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
         return msg.data;
     }
 }
 
-// ownable from context
-contract Ownable is Context  {
+
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+contract Ownable is Context {
     address private _owner;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    
-    function initialize(address sender) public initializer {
-        _owner = sender;
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () internal {
+        
+        _owner = msg.sender;
         emit OwnershipTransferred(address(0), _owner);
     }
 
-    
+    /**
+     * @dev Returns the address of the current owner.
+     */
     function owner() public view returns (address) {
         return _owner;
     }
 
-    
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
     modifier onlyOwner() {
-        require(isOwner());
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
         _;
     }
 
-    /**
-     * @return true if `msg.sender` is the owner of the contract.
-     */
-    function isOwner() public view returns (bool) {
-        return _msgSender() == _owner;
-    }
+   
+   
 
     /**
-     * @dev Allows the current owner to transfer control of the contract to a newOwner.
-     * @param newOwner The address to transfer ownership to.
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
      */
-    function transferOwnership(address newOwner) public onlyOwner {
-        _transferOwnership(newOwner);
-    }
-
-    
-    function _transferOwnership(address newOwner) internal {
-        require(newOwner != address(0));
-         _owner = newOwner;
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
         emit OwnershipTransferred(_owner, newOwner);
-       
+        _owner = newOwner;
     }
-
-    
 }
 
+
+// инициализация
+// contract Initializable {
+
+//   /**
+//    * @dev Indicates that the contract has been initialized.
+//    */
+//   bool private initialized;
+
+//   /**
+//    * @dev Indicates that the contract is in the process of being initialized.
+//    */
+//   bool private initializing;
+
+//   /**
+//    * @dev Modifier to use in the initializer function of a contract.
+//    */
+//   modifier initializer() {
+//     require(initializing || isConstructor() || !initialized, "Contract instance has already been initialized");
+
+//     bool isTopLevelCall = !initializing;
+//     if (isTopLevelCall) {
+//       initializing = true;
+//       initialized = true;
+//     }
+
+//     _;
+
+//     if (isTopLevelCall) {
+//       initializing = false;
+//     }
+//   }
+
+//   /// @dev Returns true if and only if the function is running in the constructor
+//   function isConstructor() private view returns (bool) {
+//     // extcodesize checks the size of the code stored in an address, and
+//     // address returns the current address. Since the code is still not
+//     // deployed when running a constructor, any checks on its code size will
+//     // yield zero, making it an effective way to detect if a contract is
+//     // under construction or not.
+//     uint256 cs;
+//     assembly { cs := extcodesize(address) }
+//     return cs == 0;
+//   }
+
+
+// }
 contract Emergency is Ownable{
 
   address public one;
@@ -184,34 +201,29 @@ contract Emergency is Ownable{
 
   constructor() public {
     
+    require (one != address(0) && resone != address(0), "zero address");
+    require (two != address(0) && restwo != address(0), "zero address");
+    require (three != address(0) && resthree != address(0), "zero address");
     
     one = 0x67FeE44bD5dbBAC0A9e04CcB9665077ef86303fC;
     two = 0xfdDA5b712Ae3431E0342c7686100dCF8BeE601E9;
     three = 0x7c484df5B910FE40473529F44C078aA41d794bb6;
-
-    resone = 0x67FeE44bD5dbBAC0A9e04CcB9665077ef86303fC;
-    restwo = 0xfdDA5b712Ae3431E0342c7686100dCF8BeE601E9;
-    resthree = 0x7c484df5B910FE40473529F44C078aA41d794bb6; 
-    
-    require (one != address(0) && resone != address(0), "zero address");
-    require (two != address(0) && restwo != address(0), "zero address");
-    require (three != address(0) && resthree != address(0), "zero address");
   }
 
   function Sign60() public {
-    require (msg.sender == one || msg.sender == resone);
+    require (msg.sender == one);
     require (signed[msg.sender] == false);
     signed[msg.sender] = true;
   }
 
   function Sign45() public {
-    require (msg.sender == two || msg.sender == restwo);
+    require (msg.sender == two);
     require (signed[msg.sender] == false);
     signed[msg.sender] = true;
   }
 
   function Sign15() public {
-    require (msg.sender == three || msg.sender == resthree);
+    require (msg.sender == three);
     require (signed[msg.sender] == false);
     signed[msg.sender] = true;
   }
@@ -246,10 +258,10 @@ contract PauserRole is Emergency {
     event PauserRemoved(address indexed account);
 
     Roles.Role private _pausers;
-
-    function initialize(address sender) public initializer {
-        if (!isPauser(sender)) {
-            _addPauser(sender);
+/////////////////////////////////////////////////////////////////////////////////////
+    constructor () public  {
+        if (!isPauser(msg.sender)) {
+            _addPauser(msg.sender);
         }
     }
 
@@ -296,8 +308,8 @@ contract Pausable is PauserRole {
      * @dev Initializes the contract in unpaused state. Assigns the Pauser role
      * to the deployer.
      */
-    function initialize(address sender) public initializer {
-        PauserRole.initialize(sender);
+    constructor() public  {
+        PauserRole(msg.sender);
         _paused = false;
     }
 
@@ -400,8 +412,7 @@ contract MyStream is SafeMath, Pausable{
     
      constructor() public {
         //require(cTokenManagerAddress != address(0x00), "cTokenManager contract is the zero address");
-        Ownable.initialize(msg.sender);
-        Pausable.initialize(msg.sender);
+       
         //cTokenManager = ICTokenManager(cTokenManagerAddress);
         fee = 5;
         nextStreamId = 1;
